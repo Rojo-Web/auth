f3ce47a0bafbdd63c15028bcef42caa5d24ae534
Rojo-Web <santifa2004@gmail.com>
2024-11-03 00:22:57 -0500

    Servicio en python

--- Server.py
+++ Server.py
@@ -6,3 +6,62 @@ server = Flask(__name__)
 mysql = MySQL(server)
 
 #config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_HOST")
+
+@server.route("/login",methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "Missing credentials", 401
+    
+    #revisar db para el usuario y contraseña
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email,password FROM user WHERE email=%s",(auth.username,)
+    )
+    
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        
+        if auth.username != email or auth.password != password:
+            return 'invalid credentials',401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return 'invalid credentials',401
+    
+@server.route("/validate",method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    
+    if not encoded_jwt:
+        return "missin credentials", 401
+    
+    encoded_jwt = encoded_jwt.split(" ")[1]
+    
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"),algorithms=["HS256"]
+        )
+    except:
+        return "not authorize",403
+    
+    return decoded,200
+    
+def createJWT(username,secret,authz):
+    return jwt.encode(
+        {
+            "username":username,
+            "exp":datetime.datetime.now(tz=datetime.timezone.utc)+ datetime.timedelta(days=1),
+            "iat":datetime.datetime.utcnow(),
+            "admin":authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
+
+CREATE DATABASE auth;
+GRANT ALL PRIVILEGES ON auth.* TO  'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE users (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users(email,password) VALUES ('santi@email.com','Admin123')
